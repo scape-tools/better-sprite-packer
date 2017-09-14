b2a95a3d0374f3b7c16882d809300565272fef4a
Chad <freesunfury@gmail.com>
2017-09-14 16:20:40 -0500

    Can now drag the undecorated window

--- src/io/nshusa/controller/Controller.kt
+++ src/io/nshusa/controller/Controller.kt
@@ -4,13 +4,32 @@ import javafx.application.Platform
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
 import javafx.scene.control.Label
+import javafx.scene.input.MouseEvent
 import java.net.URL
 import java.util.*
 
 class Controller : Initializable {
 
+    var offsetX = 0.0
+
+    var offsetY = 0.0
+
     override fun initialize(location: URL?, resource: ResourceBundle?) {
 
+    }
+
+    @FXML
+    fun handleMouseDragged(event: MouseEvent) {
+        var stage = App.mainStage
+
+        stage.x = event.screenX - offsetX
+        stage.y = event.screenY - offsetY
+    }
+
+    @FXML
+    fun handleMousePressed(event: MouseEvent) {
+        offsetX = event.sceneX
+        offsetY = event.sceneY
     }
 
     @FXML
