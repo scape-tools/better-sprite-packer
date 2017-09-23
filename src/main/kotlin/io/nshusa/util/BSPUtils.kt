a4d9ccf31c33ad76302b05960ad2da14049784fa
Chad <freesunfury@gmail.com>
2017-09-23 15:24:04 -0500

    Now checks for updates

--- src/main/kotlin/io/nshusa/util/BSPUtils.kt
+++ src/main/kotlin/io/nshusa/util/BSPUtils.kt
@@ -66,4 +66,34 @@ object BSPUtils {
         return ((signature[0].toInt() and 0xFF) shl 24) or ((signature[1].toInt() and 0xFF) shl 16) or ((signature[2].toInt() and 0xFF) shl 8) or (signature[3].toInt() and 0xFF) == -0x270020
     }
 
+    fun launchURL(url: String) {
+        val osName = System.getProperty("os.name")
+        try {
+            if (osName.startsWith("Mac OS")) {
+                val fileMgr = Class.forName("com.apple.eio.FileManager")
+                val openURL = fileMgr.getDeclaredMethod("openURL", String::class.java)
+                openURL.invoke(null, arrayOf<Any>(url))
+            } else if (osName.startsWith("Windows"))
+                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url)
+            else {
+                val browsers = arrayOf("firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape", "safari")
+                var browser: String? = null
+                var count = 0
+                while (count < browsers.size && browser == null) {
+                    if (Runtime.getRuntime().exec(arrayOf("which", browsers[count]))
+                            .waitFor() == 0)
+                        browser = browsers[count]
+                    count++
+                }
+                if (browser == null) {
+                    throw Exception("Could not find web browser")
+                } else
+                    Runtime.getRuntime().exec(arrayOf(browser, url))
+            }
+        } catch (ex: Exception) {
+            Dialogue.showWarning("Failed to open url.").showAndWait()
+        }
+
+    }
+
 }
