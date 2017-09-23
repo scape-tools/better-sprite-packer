80fb0686aee5d181ab74a11340f1275792040130
Chad <freesunfury@gmail.com>
2017-09-22 22:33:07 -0500

    Removed unused methods in SpritePackerUtils

--- src/main/kotlin/io/nshusa/util/SpritePackerUtils.kt
+++ src/main/kotlin/io/nshusa/util/SpritePackerUtils.kt
@@ -1,13 +1,7 @@
 package io.nshusa.util
 
-import javafx.embed.swing.SwingFXUtils
-import javafx.scene.image.Image
-import java.awt.image.BufferedImage
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
 import java.io.File
 import java.util.*
-import javax.imageio.ImageIO
 
 object SpritePackerUtils {
 
@@ -15,23 +9,9 @@ object SpritePackerUtils {
         return file.name.endsWith(".png") || file.name.endsWith(".jpg")
     }
 
-    fun toFXImage(bimage: BufferedImage) : Image {
-        return SwingFXUtils.toFXImage(bimage, null)
-    }
-
-    fun bufferedImageToGIF(bimage: BufferedImage) : BufferedImage {
-        val bos = ByteArrayOutputStream()
-
-        ImageIO.write(bimage, "gif", bos)
-
-        ByteArrayInputStream(bos.toByteArray()).use {
-            return ImageIO.read(it)
-        }
-    }
-
     fun getFilePrefix(file: File) : String {
-        var name = file.name
-        if (name.lastIndexOf(".") != -1) return name.substring(0, name.lastIndexOf(".")) else return name
+        val name = file.name
+        return if (name.lastIndexOf(".") != -1) name.substring(0, name.lastIndexOf(".")) else name
     }
 
     fun sortFiles(files: Array<File>) {
